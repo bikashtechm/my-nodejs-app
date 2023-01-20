c8d0e652e4655cd37e8160793c91a5ec2b104596
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-01-20 15:31:51 +0530

    remove gitignore files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
 
 let app: express.Application = express();
 
@@ -11,7 +12,23 @@ app.listen(5000, () => {
   console.log("Server is running at port:5000");
 });
 
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@mongodb.f7h21rd.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("MongoDB is connected...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.get("/api/user/login", (req: any, res) => {
   const data = [{ firstName: "Bikash", lastName: "Shaw", mobile: 9096028231 }];
   res.status(200).send(data);
+});
+
+app.get("/api/user/signup", (req, res) => {
+  res.status(200).send("Signup API called..");
 });
